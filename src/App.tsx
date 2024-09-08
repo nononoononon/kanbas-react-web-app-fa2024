671b24a853f588dff541d215e21234ff47d377fb
nonononoonononon <yuangli971@gmail.com>
2024-09-08 08:32:34 -0400

    intial

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,24 @@ import './App.css';
 import Labs from "./Labs";
 
 function App() {
-  return (
-      <div>
-        <Labs/>
-      </div>
-
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
