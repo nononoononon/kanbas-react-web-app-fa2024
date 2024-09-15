35bfc5465b17b26d71b0df029dd64da6daf40cfa
nonononoonononon <yuangli971@gmail.com>
2024-09-14 23:01:53 -0400

    add name in labs

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,6 +7,7 @@ export default function Labs() {
     return (
         <div>
             <h1>Labs</h1>
+            <h2>Name : Yuang Li</h2>
             <TOC />
             <Routes>
                 <Route path="/" element={<Navigate to="Lab1" />} />
